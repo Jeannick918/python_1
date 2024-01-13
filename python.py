c0aa7052180599e38a7b9d3fff866136e5b1b5e6
Jeannick918 <jeannickfranze@email.com>
2024-01-13 17:50:27 +0100

    Added new file

---
+++ python.py
@@ -0,0 +1,7 @@
+x = 16
+y = 9
+z = x + y
+print(z)
+
+h = "To protect us from abuse, we validate all email addresses are valid. If you check your email now, it should already be there. If you can't see it, check spam, and then give it 10 minutes in case things are running slowly."
+print(h.upper())
